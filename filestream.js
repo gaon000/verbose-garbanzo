d5ac3513c0af21faf5f18aabc7a7bab267e87986
unknown <gaon000@naver.com>
2021-06-02 15:33:31 +0900

    Add file name encoder

--- filestream.js
+++ filestream.js
@@ -1,13 +1,22 @@
 const fs = require('fs')
 const http = require('http')
 const server = http.createServer()
+const url = require('url')
+const getDownloadFilename = require('./filenameConverter').getDownloadFilename
 
 // just plain txt file
 server.on("request", (req, res) => {
-  // res.setHeader('Content-Type', 'text/plain')
-  res.setHeader('Content-disposition', 'attachment; filename=hello.txt')
-  const src = fs.createReadStream("./hello.txt")
-  src.pipe(res)
+  // res.setHeader('Content-Type', 'multipart/formed-data')
+  const parsed_url = url.parse(req.url)
+  if (req.url == '/hello') { 
+    res.setHeader('Content-disposition', 'attachment; filename=' + getDownloadFilename(req, '안녕.txt'))
+    const src = fs.createReadStream("./hello.txt")
+    src.pipe(res)
+  }
+  else {
+    res.statusCode = 404
+    res.end('bye')
+  }
 })
 
 server.listen(8000)
